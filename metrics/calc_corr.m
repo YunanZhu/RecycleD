6e078829d1e80ef6b7a18696c1ab2c4104a5f782
Yunan Zhu <49200936+YunanZhu@users.noreply.github.com>
2021-08-13 00:47:57 +0800

    Update calc_corr.m

--- metrics/calc_corr.m
+++ metrics/calc_corr.m
@@ -1,10 +1,10 @@
-%% Configure the score lists.
+% Configure the score lists.
 % You can copy the data from the excel and paste them at Matlab workspace.
 % Then rename the table to 'T' and rename the colName to 'pred' & 'mos'. 
 pred = T.pred; % Set the predicted score.
 mos = T.mos; % Set the MOS.
 
-%% Calculate PLCC, SRCC and KRCC.
+% Calculate PLCC, SRCC and KRCC.
 f = polyfit(pred, mos, 3);
 fitted_mos = polyval(f, pred);
 fitted_PLCC = abs(corr(fitted_mos, mos, 'type', 'Pearson'));
